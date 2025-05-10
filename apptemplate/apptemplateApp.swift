c3aa5c0ea5d3b64455adbf096d9b067ab0f0e383
Rushil <121330302+mxyxyz9@users.noreply.github.com>
2025-05-10 15:57:38 +0530

    Initial Commit

---
+++ apptemplate/apptemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  apptemplateApp.swift
+//  apptemplate
+//
+//  Created by Pala Rushil on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct apptemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
